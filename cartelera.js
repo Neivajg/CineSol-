e7eb8448f9b0781829fe8fe4b6aeafa7aaae9301
Pipe Rucinque <luis.felipe.rucinque@gmail.com>
2022-11-03 15:18:03 +0100

    posters agregados al DOM

--- cartelera.js
+++ cartelera.js
@@ -6,8 +6,12 @@ const carteleraPeliculas = async function () {
     const respond = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=b9497f6dddc3b0606f3559d28ee7629a")
     const pelicula = await respond.json()
 
+    let img = document.querySelectorAll(".grid img");
+
         for (let i = 0; i < 6; i++) {
             console.log(pelicula.results[i].poster_path)
+
+            img[i].src = `https://image.tmdb.org/t/p/w500${pelicula.results[i].poster_path}`;
             
         }
 
